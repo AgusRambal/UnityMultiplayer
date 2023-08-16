7fa441da10559fe1d54ec0e7964d4a57ee3ea10a
AgustinRambaldo <73607427+AgusRambal@users.noreply.github.com>
2023-08-16 11:07:44 -0300

    Coin spawner

--- Assets/Scripts/Core/Coins/RespawningCoin.cs
+++ Assets/Scripts/Core/Coins/RespawningCoin.cs
@@ -1,7 +1,22 @@
+using System;
 using UnityEngine;
 
 public class RespawningCoin : Coin
 {
+    public event Action<RespawningCoin> OnCollected;
+
+    private Vector3 previousPosition;
+
+    private void Update()
+    {
+        if (previousPosition != transform.position)
+        {
+            Show(true);
+        }
+
+        previousPosition = transform.position;
+    }
+
     public override int Collect()
     {
         if (!IsServer)
@@ -15,6 +30,13 @@ public class RespawningCoin : Coin
 
         alreadyCollected = true;
 
+        OnCollected?.Invoke(this);
+
         return coinValue;
+    }
+
+    public void Reset()
+    {
+        alreadyCollected = false;
     }
 }

5c2e1c0fa136f114fac44e8ba2401fcd3a18f4aa
AgustinRambaldo <73607427+AgusRambal@users.noreply.github.com>
2023-08-30 13:00:15 -0300

    Sounds fixed

--- Assets/Scripts/Sound/OnClickSFX.cs
+++ Assets/Scripts/Sound/OnClickSFX.cs
@@ -1,20 +1,36 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OnClickSFX : MonoBehaviour
 {
     [SerializeField] private AudioClip SFX;
+    [SerializeField] private List<AudioClip> SFXs = new List<AudioClip>();
     [SerializeField] private AudioSource AudioSource;
 
     public bool playAtStart = false;
+    public bool useRandomList = false;
 
     private void Start()
     {
         if (playAtStart)
-            EventManager.TriggerEvent(GenericEvents.PlaySound, new Hashtable() {
-            {GameplayEventHashtableParams.AudioClip.ToString(), SFX},
-            {GameplayEventHashtableParams.AudioSource.ToString(), AudioSource}
-            });
+        {
+            if (useRandomList)
+            {
+                EventManager.TriggerEvent(GenericEvents.PlaySound, new Hashtable() {
+                {GameplayEventHashtableParams.AudioClip.ToString(), SFXs[Random.Range(0, SFXs.Count)]},
+                {GameplayEventHashtableParams.AudioSource.ToString(), AudioSource}
+                });
+            }
+
+            else
+            {
+                EventManager.TriggerEvent(GenericEvents.PlaySound, new Hashtable() {
+                {GameplayEventHashtableParams.AudioClip.ToString(), SFX},
+                {GameplayEventHashtableParams.AudioSource.ToString(), AudioSource}
+                });
+            }
+        }
     }
 
     //Button only
